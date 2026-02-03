cab6eacb662df226c79fefb1425c4efb6bbf2ec1
Gabriel Bastos Sobral <gabastos@c1r8p3.42sp.org.br>
2026-02-03 11:38:41 -0300

    solving problem at register

--- app/frontend/js/views/register.js
+++ app/frontend/js/views/register.js
@@ -42,6 +42,7 @@ const RegisterView = {
             const username = document.getElementById('username').value;
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
+            const confirmPassword = document.getElementById('confirm-password').value;
 
             if (password !== confirmPassword) {
                 alert('Passwords do not match.');
